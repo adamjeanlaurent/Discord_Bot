f387123b32e86f7f681c0391a43fca66cb85d736
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2020-03-13 17:39:34 -0400

    basic message and config

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require('./config.json');
 
+/*
+    To-Do: 
+    - Make the token a secret so you can put this on github
+    
+*/
+
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.login('your-token-goes-here');
+client.on('message', message => {
+    if(message.content === 'Hello Carl') {
+        message.channel.send('Test');
+    }
+});
+
+client.login(config.token);
